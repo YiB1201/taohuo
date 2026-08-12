2b8a53f003225ba994c32ebae3dd9af243631e75
YiB1201 <2313964396@qq.com>
2026-08-12 14:15:14 +0800

    chore(version): 更新版本号到2.6
    - 将versionName从2.5更新为2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 27
         targetSdk = 36
         versionCode = 1
-        versionName = "2.5"
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
